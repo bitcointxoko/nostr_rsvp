f81d8c951a58210fe45bccc914bec1a4ccdbbfc1
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2023-11-17 16:54:09 +0100

    Update RsvpEntry.jsx

--- src/components/RsvpEntry.jsx
+++ src/components/RsvpEntry.jsx
@@ -38,7 +38,7 @@ function RsvpEntry({ message }) {
       ) : (
         <>
           <span>{username}</span>
-          <small className="ml-1">(You)</small>
+          <small className="ml-1">(Tú)</small>
         </>
       )}
       {numberOfPlusOnes.length > 0 && parseInt(numberOfPlusOnes[0][1]) > 0 && (
