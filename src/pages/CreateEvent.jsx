4fb5f67eeaa337c099196e4d540ffa050f9d2585
Bitcoin Txoko <142011724+bitcointxoko@users.noreply.github.com>
2023-11-17 17:07:24 +0100

    Update CreateEvent.jsx

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -55,20 +55,20 @@ export default function CreateEvent() {
   return (
     <div className="mt-8 mb-8 border-b md:mt-16 md:mb-16">
       <h1 className="px-4 mb-4 font-serif text-3xl font-bold md:text-5xl text-sky-700">
-        Surprise Visitors? <br />
-        Only if They're Bringing Pizza!
+        ¿Visitantes sorpresa? <br />
+        ¡Sólo si traen pizza!
       </h1>
       <p className="max-w-xl px-5 mb-4 text-lg text-gray-700">
-        A simple RSVP Solution for receiving your event confirmations and
-        keeping track of who is coming.
+        Una sencilla solución de RSVP para recibir las confirmaciones de tus eventos y
+        saber quién va a venir.
       </p>
       <strong className="block px-4 pt-4 pb-2 font-serif text-xl text-gray-700">
-        Create your first Event:
+        Crea tu primer Evento:
       </strong>
       <form
         className="flex flex-col grid-cols-3 gap-2 md:grid card"
         onSubmit={submitHandler}>
-        <label>Your Name:</label>
+        <label>Tu nombre:</label>
         <input
           onChange={(e) => setUsername(e.target.value)}
           value={username}
@@ -76,13 +76,13 @@ export default function CreateEvent() {
           placeholder="John Doe"
           required
         />
-        <label>Event Name:</label>
+        <label>Nombre del evento:</label>
         <input
           onChange={(e) =>
             setEvent((prev) => ({ ...prev, name: e.target.value }))
           }
           value={event.name}
-          placeholder="My Birthday Party"
+          placeholder="Mi fiesta de cumpleaños"
           className="col-span-2 mb-2 input"
           required
         />
@@ -95,7 +95,7 @@ export default function CreateEvent() {
           className="input"
         /> */}
 
-        <label>Event Start Date:</label>
+        <label>Fecha de inicio del evento:</label>
         <input
           type="datetime-local"
           onChange={(e) => handleDateChange(e, "start")}
@@ -110,23 +110,23 @@ export default function CreateEvent() {
           className="input"
         /> */}
 
-        <label>Event Location:</label>
+        <label>Lugar del evento:</label>
         <input
           onChange={(e) =>
             setEvent((prev) => ({ ...prev, location: e.target.value }))
           }
           value={event.location}
           className="col-span-2 mb-2 input"
-          placeholder="My House"
+          placeholder="Mi casa"
           required
         />
         <div />
         <small className="col-span-2 pb-2 -mt-2 font-sans md:px-4 text-stone-600">
-          Please don't share information that you'd like to keep private.
+          Por favor, no compartas información que quieras mantener en privado.
         </small>
         <div className="col-span-2">
           <button className="text-left text-white bg-amber-600 input">
-            Create Event
+            Crear Evento
           </button>
         </div>
       </form>
